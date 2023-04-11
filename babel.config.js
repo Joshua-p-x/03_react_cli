873007c67c6e71def7d4a8b118e3ae85af0e7abf
joshua.xu <2523892857@qq.com>
2023-04-11 22:31:14 +0800

    inti: react app init

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // https://github.com/facebook/create-react-app/blob/main/packages/babel-preset-react-app/create.js#L178
+  // babel-preset-crate-app
+  // 其中已经做了@babel/plugin-transform-runtime的配置
+  presets: ['react-app'],
+};
